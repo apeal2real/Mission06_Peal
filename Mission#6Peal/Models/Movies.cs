0a1fec7d70ac4b52078d3d5bb2266746c16fb10f
apeal2real <alec.peal@gmail.com>
2024-02-16 21:22:42 -0700

    Add string limit

--- Mission#6Peal/Models/Movies.cs
+++ Mission#6Peal/Models/Movies.cs
@@ -13,6 +13,7 @@ namespace Mission_6Peal.Models
         public string Rating { get; set; }
         public string? Edited { get; set; }
         public string? LentTo { get; set; }
+        [StringLength(25)]
         public string? Notes { get; set; }
 
 
